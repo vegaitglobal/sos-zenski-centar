9adf1b8e2efe75d01188e6ed22ec3f57c8404682
Sasa Stevcic <s.stevcic@vegait.rs>
2021-09-24 21:07:29 +0200

    Add assets

--- sos-web/src/components/atoms/Icon/Icon.js
+++ sos-web/src/components/atoms/Icon/Icon.js
@@ -1,5 +1,23 @@
+import { ReactComponent as AddComment } from './svg/add-comment.svg';
+import { ReactComponent as ArrowDown } from './svg/arrow-down.svg';
+import { ReactComponent as ArrowUp } from './svg/arrow-up.svg';
+import { ReactComponent as Close } from './svg/close.svg';
+import { ReactComponent as Comment } from './svg/comment.svg';
+import { ReactComponent as Edit } from './svg/edit.svg';
 import { ReactComponent as Logo } from './svg/logo.svg';
+import { ReactComponent as Plus } from './svg/plus.svg';
+import { ReactComponent as Report } from './svg/report.svg';
+import { ReactComponent as Wave } from './svg/wave.svg';
 
 export const Icon = {
+  AddComment,
+  ArrowDown,
+  ArrowUp,
+  Close,
+  Comment,
+  Edit,
   Logo,
+  Plus,
+  Report,
+  Wave,
 };
--- sos-web/src/components/pages/Home.js
+++ sos-web/src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import { Heading } from '../atoms/Heading/Heading';
+import { Icon } from '../atoms/Icon/Icon';
 import { Container } from '../layout/Container';
 
 export const Home = () => {
   return (
     <Container>
       <Heading>Home</Heading>
+      <Icon.Report />
     </Container>
   );
 };
