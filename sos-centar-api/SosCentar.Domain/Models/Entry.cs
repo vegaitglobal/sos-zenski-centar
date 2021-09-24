a2ed7b43d7d8b89577b4491db41e8fd8ab1b9e1a
Uros Vukovic <u.vukovic@vegait.rs>
2021-09-24 22:21:14 +0200

    Add models

---
+++ sos-centar-api/SosCentar.Domain/Models/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SosCentar.Domain.Models
+{
+    public class Entry
+    {
+        public Guid Id { get; set; }
+        public User User { get; set; }
+        public DateTime Date { get; set; }
+        public IEnumerable<SubmitedAnswer> SubmitedAnswers { get; set; }
+    }
+}
