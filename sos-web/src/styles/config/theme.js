376e95b64dedd0aea5f120813c89be0e49228df7
Sasa Stevcic <s.stevcic@vegait.rs>
2021-09-24 20:48:45 +0200

    Add theme colors

--- sos-web/src/styles/config/theme.js
+++ sos-web/src/styles/config/theme.js
@@ -5,6 +5,14 @@ export const color = {
   whiteFaded: rgba('#fff', 0.5),
   black: '#000',
   blackFaded: rgba('#000', 0.5),
+  grey: '#676767',
+  greyLight: '#ebebeb',
+  pink: '#d8a4d9',
+  pinkLight: '#faf0fc',
+  pinkLighter: '#faf0fc',
+  purple: '#57415f',
+  orange: '#ffa480',
+  orangeLight: '#ffe0d0',
 };
 
 /* styled-components theme */
