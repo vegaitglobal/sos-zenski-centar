e4af8a845790f1f4238d383585a36b497a9cc3b1
Uros Vukovic <u.vukovic@vegait.rs>
2021-09-24 22:30:52 +0200

    fix/imports removed

--- sos-centar-api/SosCentar.Domain/Models/Category.cs
+++ sos-centar-api/SosCentar.Domain/Models/Category.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SosCentar.Domain.Models
 {
@@ -11,6 +7,5 @@ namespace SosCentar.Domain.Models
         public Guid Id { get; set; }
         public string Name { get; set; }
         public int OrderBy { get; set; }
-
     }
 }
