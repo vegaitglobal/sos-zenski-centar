a6f02bdad96935fc0d8004033f74d7c92c05d984
Milan Bunjevcev <m.bunjevcev@vegait.rs>
2021-09-24 23:09:02 +0200

    add base security

---
+++ sos-centar-api/SosCentar.API/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SosCentar.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public string TestIt()
+        {
+            return "test";
+        }
+
+        [HttpGet("login")]
+        [AllowAnonymous]
+        public string Login()
+        {
+            return "AllowAnonymous";
+        }
+    }
+}
--- sos-centar-api/SosCentar.API/Startup.cs
+++ sos-centar-api/SosCentar.API/Startup.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
 
 namespace SosCentar.Api
 {
@@ -21,6 +25,23 @@ namespace SosCentar.Api
         {
 
             services.AddControllers();
+
+            services.AddAuthorization();
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateAudience = false,
+                        ValidateIssuer = false,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SosCentarAPI", Version = "v1" });
