9b8a4463cc437f2e4c2a3f391c381ec6f726af56
Srdjan Stankov <s.stankov@vegait.rs>
2021-09-25 23:20:17 +0200

    Add methods to interface IReportService

--- sos-centar-api/SosCentar.BusinessLogic/Services/ReportsService.cs
+++ sos-centar-api/SosCentar.BusinessLogic/Services/ReportsService.cs
@@ -1,8 +1,18 @@
-﻿using SosCentar.Contracts.Interfaces.Services;
+﻿using SosCentar.Contracts.Dtos.ReportTables;
+using SosCentar.Contracts.Interfaces.Services;
 
 namespace SosCentar.BusinessLogic.Services
 {
-    public class ReportsService : IReportService
-    {
-    }
+	public class ReportsService : IReportService
+	{
+		public object GetGraphs()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public Table GetTableReport()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
 }
--- sos-centar-api/SosCentar.Contracts/Interfaces/Services/IReportService.cs
+++ sos-centar-api/SosCentar.Contracts/Interfaces/Services/IReportService.cs
@@ -1,5 +1,10 @@
-﻿namespace SosCentar.Contracts.Interfaces.Services
+﻿using SosCentar.Contracts.Dtos.ReportTables;
+
+namespace SosCentar.Contracts.Interfaces.Services
 {
     public interface IReportService
-    { }
+    {
+        Table GetTableReport();
+        object GetGraphs();
+    }
 }
