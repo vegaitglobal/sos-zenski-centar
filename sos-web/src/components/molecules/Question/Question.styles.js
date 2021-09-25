c23dd8650f58b32b643752360af1a15bd24d33fc
n.porobic@vegait.rs <n.porobic@vegait.rs>
2021-09-25 16:50:11 +0200

    Question component added

---
+++ sos-web/src/components/molecules/Question/Question.styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const StyledQuestion = styled.div``;
+
+export const QuestionTitle = styled.h1`
+  color: ${({ theme }) => theme.color.purple};
+  margin-bottom: 20px;
+`;
