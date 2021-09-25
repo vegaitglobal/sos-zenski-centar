ca6f0748298d7a1f7cebacb4f30eba9b0ba3b1d3
Milan Bunjevcev <m.bunjevcev@vegait.rs>
2021-09-25 22:29:06 +0200

    implement get category

--- sos-centar-api/SosCentar.BusinessLogic/Services/CategoryService.cs
+++ sos-centar-api/SosCentar.BusinessLogic/Services/CategoryService.cs
@@ -1,36 +1,74 @@
-﻿using SosCentar.Contracts.Dtos.Categories;
+﻿using SosCentar.Contracts.Dtos.Answers;
+using SosCentar.Contracts.Dtos.Categories;
+using SosCentar.Contracts.Dtos.QuestionConditions;
+using SosCentar.Contracts.Dtos.Questions;
+using SosCentar.Contracts.Dtos.Sections;
 using SosCentar.Contracts.Interfaces.Repositories;
 using SosCentar.Contracts.Interfaces.Services;
+using SosCentar.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace SosCentar.BusinessLogic.Services
 {
-	public class CategoryService : ICategoryService
-	{
-		private ICategoryRepository _categoryRepository;
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
 
-		public CategoryService(ICategoryRepository categoryRepository)
-		{
-			_categoryRepository = categoryRepository;
-		}
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
 
-		public IEnumerable<CategoryListDto> GetAll()
-		{
-			return _categoryRepository.GetAllCategories()
-				.OrderBy(category => category.Order)
-				.Select(category => new CategoryListDto() { Id = category.Id, Label = category.Name });
-		}
+        public IEnumerable<CategoryListDto> GetAll()
+        {
+            return _categoryRepository.GetAllCategories()
+                .OrderBy(category => category.Order)
+                .Select(category => new CategoryListDto() { Id = category.Id, Label = category.Name });
+        }
 
-		public CategoryDto GetById(Guid id)
-		{
-			var category = _categoryRepository.GetById(id);
+        public CategoryDto GetById(Guid id)
+        {
+            var category = _categoryRepository.GetById(id);
 
-			return new CategoryDto()
-			{
+            var sortedSectionDtos = category.Sections
+                .OrderBy(section => section.Order)
+                .Select(GetSectionDto);
 
-			};
-		}
-	}
+            return null;
+        }
+
+        private SectionDto GetSectionDto(Section section)
+        {
+            return new SectionDto()
+            {
+                Id = section.Id,
+                SectionName = section.Name,
+                Questions = section.Questions.OrderBy(question => question.Order).Select(GetQuestionDto)
+            };
+        }
+
+        private QuestionDto GetQuestionDto(Question question)
+        {
+            return new QuestionDto()
+            {
+                Id = question.Id,
+                Icon = question.Icon,
+                Label = question.Text,
+                IsRequired = question.IsRequired,
+                Condition = new QuestionConditionDto() { AnswerId = question.QuestionCondition.AnswerId, QuestionId = question.QuestionCondition.QuestionIdMaster },
+                Options = question.Answers.OrderBy(answer => answer.Order).Select(GetAnswerDto)
+            };
+        }
+
+        private AnswerDto GetAnswerDto(Answer answer)
+        {
+            return new AnswerDto()
+            {
+                Id = answer.Id,
+                Label = answer.Text
+            };
+        }
+    }
 }
