5651632f2c99b8397e34c3383c4789d221609c18
Vuk Vlahovic <v.vlahovic@vega.local>
2021-09-25 22:26:52 +0200

    Data seed script v1

--- sos-centar-api/seed-data.sql
+++ sos-centar-api/seed-data.sql
@@ -1,12 +1,4 @@
-﻿CREATE TABLE IF NOT EXISTS "Users"
-(
-	"Id" uuid NOT NULL,
-	"Email" text,
-	"FirstName" text,
-	"LastName" text,
-	"Password" text
-);
-INSERT INTO "Users" (
+﻿INSERT INTO "Users" (
 	"Id",
 	"Email",
     "FirstName",
@@ -285,4 +277,219 @@ VALUES
 		'Zorica',
 		'Pajantić',
 		'pHm1f1ZbrCNDN4mY8rFnCnX1VME/5GashWKfSw4U8eQ='
-	)
+	);
+
+INSERT INTO "Categories" (
+	"Id",
+	"Name",
+    "Order"
+)
+VALUES 
+	(
+		'87d01ea9-7ba2-42ae-ba82-c6e77aff3879',
+		'SOS telefon',
+		'1'
+	),
+	(
+		'ed3a2a18-c8af-4064-b5b0-c7d9c1e9c47e',
+		'SOS poruke',
+		'2'
+	),
+	(
+		'02900ad4-2d5c-4dc2-aa98-5e366d9d5c05',
+		'Psiholosko savetovanje',
+		'3'
+	),
+	(
+		'42ed53bb-58d8-4a6f-ae0c-52486b0b6e0c',
+		'Omladinsko savetovanje',
+		'4'
+	),
+	(
+		'e212a66f-e951-48b7-bb34-720e147264c2',
+		'Pravna pomoc',
+		'5'
+	);
+
+INSERT INTO "Sections" (
+	"Id",
+	"Name",
+	"CategoryId",
+    "Order"
+)
+VALUES
+	(
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2',
+		'Opis usluge',
+		'87d01ea9-7ba2-42ae-ba82-c6e77aff3879',
+		'1'
+	),
+	(
+		'cb428035-1089-4026-a39b-925560ab6615',
+		'Opis usluge',
+		'ed3a2a18-c8af-4064-b5b0-c7d9c1e9c47e',
+		'2'
+	),
+	(
+		'0da678bd-9110-4dad-97cf-a531ceef1622',
+		'Opis usluge',
+		'02900ad4-2d5c-4dc2-aa98-5e366d9d5c05',
+		'3'
+	),
+	(
+		'7875c455-6ac9-4daa-a75b-761b0845449c',
+		'Opis usluge',
+		'02900ad4-2d5c-4dc2-aa98-5e366d9d5c05',
+		'4'
+	),
+	(
+		'64f02514-cac6-4563-b1b7-954743c7c05f',
+		'Opis usluge',
+		'e212a66f-e951-48b7-bb34-720e147264c2',
+		'5'
+	),
+	
+	(
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f',
+		'Poziv',
+		'87d01ea9-7ba2-42ae-ba82-c6e77aff3879',
+		'1'
+	),
+	(
+		'393de121-fcff-4932-bb46-606e4509a302',
+		'Poziv',
+		'ed3a2a18-c8af-4064-b5b0-c7d9c1e9c47e',
+		'2'
+	),
+	(
+		'b3abe820-f54a-47d3-9882-e6428612e5fc',
+		'Poziv',
+		'02900ad4-2d5c-4dc2-aa98-5e366d9d5c05',
+		'3'
+	),
+	(
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4',
+		'Poziv',
+		'02900ad4-2d5c-4dc2-aa98-5e366d9d5c05',
+		'4'
+	),
+	(
+		'a8e718f6-6832-4e57-b387-3349a99cce32',
+		'Poziv',
+		'e212a66f-e951-48b7-bb34-720e147264c2',
+		'5'
+	),
+
+	(
+		'8e445aa1-4587-4433-824b-f0ff900f840e',
+		'Intervencije',
+		'87d01ea9-7ba2-42ae-ba82-c6e77aff3879',
+		'1'
+	),
+	(
+		'40fdd452-3837-40e5-a1d5-6a6f7774f838',
+		'Intervencije',
+		'ed3a2a18-c8af-4064-b5b0-c7d9c1e9c47e',
+		'2'
+	),
+	(
+		'8f72b1e0-7a29-4659-b1a1-89c247e96392',
+		'Intervencije',
+		'02900ad4-2d5c-4dc2-aa98-5e366d9d5c05',
+		'3'
+	),
+	(
+		'7d59dc7a-5d64-4210-8446-53c885b945cc',
+		'Intervencije',
+		'02900ad4-2d5c-4dc2-aa98-5e366d9d5c05',
+		'4'
+	),
+	(
+		'08052000-470a-4bf9-8809-b1bf7b45d528',
+		'Intervencije',
+		'e212a66f-e951-48b7-bb34-720e147264c2',
+		'5'
+	),
+
+
+
+
+	INSERT INTO "Questions" (
+	"Id",
+	"Text",
+	"Order",
+	"IsRequired",
+    "SectionId"
+)
+	(
+		'ef2f3526-b26a-4ef7-8589-46eb74f64521',
+		'Kratak opis problema',
+		'1'
+		'1'
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'103fa35e-2d8a-42a3-8fb6-3cf9d77f6b73',
+		'Kratak opis problema',
+		'2'
+		'1'
+		'cb428035-1089-4026-a39b-925560ab6615'
+	),
+	(
+		'd98d5788-3c69-44a4-af79-c17dd2955ff9',
+		'Kratak opis problema',
+		'3'
+		'1'
+		'0da678bd-9110-4dad-97cf-a531ceef1622'
+	),
+	(
+		'53fddf21-0474-4fc9-a328-91e12da78701',
+		'Kratak opis problema',
+		'4'
+		'1'
+		'7875c455-6ac9-4daa-a75b-761b0845449c'
+	),
+	(
+		'753ebc33-be72-4b13-a3be-e4ec71e63821',
+		'Kratak opis problema',
+		'5'
+		'1'
+		'64f02514-cac6-4563-b1b7-954743c7c05f'
+	),
+	(
+		'ee3b6d28-de62-4a60-8894-56b1650a0388',
+		'Tip problema',
+		'1'
+		'1'
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'b188cc90-2ee8-4762-a7c8-7da452ecf1cb',
+		'Tip problema',
+		'2'
+		'1'
+		'cb428035-1089-4026-a39b-925560ab6615'
+	),
+	(
+		'0da678bd-9110-4dad-97cf-a531ceef1622',
+		'Tip problema',
+		'3'
+		'1'
+		'0da678bd-9110-4dad-97cf-a531ceef1622'
+	),
+	(
+		'993a4e22-69d6-4867-b121-cc0b7b2c3851',
+		'Tip problema',
+		'4'
+		'1'
+		'7875c455-6ac9-4daa-a75b-761b0845449c'
+	),
+	(
+		'44e2620d-8226-4267-9ddc-79ec3c116743',
+		'Tip problema',
+		'5'
+		'1'
+		'64f02514-cac6-4563-b1b7-954743c7c05f'
+	),
+
+
