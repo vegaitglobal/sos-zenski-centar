a73fbe22a8cb7ea888696d4bb857b6221a1b1fbc
Uros Vukovic <u.vukovic@vegait.rs>
2021-09-25 17:47:57 +0200

    New migrations and database seed

--- sos-centar-api/seed-data.sql
+++ sos-centar-api/seed-data.sql
@@ -1,279 +1,288 @@
-﻿INSERT INTO Users (
-    first_name,
-    last_name,
-    email,
-    password
+﻿CREATE TABLE IF NOT EXISTS "Users"
+(
+	"Id" uuid NOT NULL,
+	"Email" text,
+	"FirstName" text,
+	"LastName" text,
+	"Password" text
+);
+INSERT INTO "Users" (
+	"Id",
+	"Email",
+    "FirstName",
+    "LastName",
+    "Password"
 )
 VALUES
     
 	(
+		'54cfe685-872b-455f-8031-a58d6283369b',
 		'vanjastepanovic001@gmail.com',
 		'Vanja',
 		'Stepanović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'f5f612e1-b3ed-40f1-b876-f76f39871357',
 		'noapernoa@gmail.com',
 		'Radojka',
 		'Jevtić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'51bf1297-194d-40c6-a99d-d66db8a809c2',
 		'vtesan95@gmail.com',
 		'Ivana',
 		'Tešan',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'4565a186-734e-4574-a09d-a0659dc35a0f',
 		'vanjatvanja@gmail.com',
 		'Vanja',
 		'Trkulja',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'2383ea5d-9fbb-4f88-b34c-0a391c4d113d',
 		'maja.pistolic@gmail.com',
 		'Maja',
 		'Pistolić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'417bb1f4-3326-4f57-80a1-ac18e419600c',
 		'jovana.kostevski@gmail.com',
 		'Jovana',
 		'Kostevski',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'68606979-0ee2-4a74-bc71-ffa60bd4e524',
 		'ivana.nesovic@sosns.rs',
 		'Ivana',
 		'Nešović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'2057c635-ae1a-4731-95b4-df599afdf39c',
 		'teodora.tepic@sosns.rs',
 		'Teodora',
 		'Tepić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'556e6a45-0316-4105-ac85-48fd2ffbc2a5',
 		'jovanaradulovic22@gmail.com',
 		'Jovana',
 		'Radulović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'6645a55b-efe3-4802-952c-04c0964637a7',
 		'andri.kosanin@gmail.com',
 		'Andrea',
 		'Košanin',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'd0dacce6-532f-4e47-becd-3e411cb918a2',
 		'jelenastojanovic@sosns.rs',
 		'Jelena',
 		'Stojanović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'20dad719-8261-475a-b42a-9958df0c6635',
 		'cupcake.nin@gmail.com',
 		'Nina',
 		'Racić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'1253b52d-79d9-4ed4-8f7b-2b3756f8412a',
 		'saska.milovac795@gmail.com',
 		'Aleksandra',
 		'Dukić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'0e44ad7f-f2ad-4763-a9c8-33599bccc201',
 		'oliverakovacev@yahoo.com ',
 		'Olivera',
 		'Kovačev',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'0639479b-6274-4812-a1bd-52146e6b5852',
 		'sos.telefon@gmail.com',
 		'Ivana',
 		'Perić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'32967b37-d816-48e6-b10b-51193012ebb9',
 		'masapoznanovic@sosns.rs',
 		'Maša',
 		'Poznanović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'b4fb20d0-8de3-4302-a79e-5b77d4d3e4f1',
 		'biljanagalic@sosns.rs',
 		'Biljana',
 		'Galić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'2f1f656b-233f-47d2-8029-5a96348d3cb1',
 		'psihologanakusturic@gmail.com',
 		'Ana',
 		'Todorčević',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'cad75e4e-bde5-4e76-902b-2c83c353fbf0',
 		'dusicasostaric@yahoo.com',
 		'Dušica',
 		'Šoštarić Blagojević',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'030520ae-4b47-4789-8bb0-149c609724e9',
 		'mirela.pavlovic@gmail.com',
 		'Mirela',
 		'Pavlović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'a052870c-214d-4bd9-94ad-af8674e4b28d',
 		'nedasubotic21@gmail.com',
 		'Neda',
 		'Subotić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'4cec45db-24b8-4d0d-a14a-db9df0613b5e',
 		'aleksandracurcic16@gmail.com ',
 		'Aleksandra',
 		'Ćurčić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'd4783ab2-1d6b-4382-9ee7-a4e42ebcb475',
 		'jovana.ozimkovic@gmail.com',
 		'Jovana',
 		'Ozimković',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'71431e91-b96d-45c0-be15-4c6cc948207c',
 		'pejicsilvana@gmail.com',
 		'Silvana',
 		'Pejić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'ed7c35a4-7373-426f-b0d0-953093c49d24',
 		'sandrafilipovic998@gmail.com ',
 		'Sandra',
 		'Filipović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'42b2893a-b371-4a22-be3f-dd5d5e8cab8a',
 		'saska.bajic86@gmail.com ',
 		'Aleksandra',
 		'Bajić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'60421eb3-8ab3-4687-ae02-b893f277b9c2',
 		'tanja.psychology@gmail.com',
 		'Tanja',
 		'Adamović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'ad7260e6-cc67-4017-b149-79e21a4ea8ba',
 		'zlatkovicaleksandra2@gmail.com',
 		'Aleksandra',
 		'Zlatković',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'10587256-8d53-4e89-87d7-80324ef118af',
 		'milenapaunkovic@hotmail.com',
 		'Milena',
 		'Paunković',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'66ba82af-ac96-4167-8092-d54839bcf52b',
 		'milica.ludoski95@gmail.com',
 		'Milica',
 		'Ludoški',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'df692930-0771-4188-a6ca-bcad1a6bdbb1',
 		'zorana.damjanovic.savetovanje@gmail.com ',
 		'Zorana',
 		'Damjanović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'244d1d6d-f958-4f03-8ae6-f2188fe71a82',
 		'mdespotovic996@gmail.com',
 		'Maja',
 		'Despotović',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'e6de7d65-f728-4371-9a60-4117d6058f74',
 		'nedasubotic21@gmail.com ',
 		'Neda',
 		'Subotić ',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	),
 	
 	(
+		'1d5d2b5d-9732-4a8f-843d-fed870267504',
 		'nszorica@gmail.com',
 		'Zorica',
 		'Pajantić',
 		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
-		
 	)
