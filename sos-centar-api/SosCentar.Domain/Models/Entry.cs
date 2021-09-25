d060cbbac95a2fb89880702f5552b1338eb7097a
Milan Bunjevcev <m.bunjevcev@vegait.rs>
2021-09-26 00:08:03 +0200

    Add category id to entry

--- sos-centar-api/SosCentar.Domain/Models/Entry.cs
+++ sos-centar-api/SosCentar.Domain/Models/Entry.cs
@@ -7,6 +7,7 @@ namespace SosCentar.Domain.Models
     {
         public Guid Id { get; set; }
         public string Description { get; set; }
+        public Category Category { get; set; }
         public User User { get; set; }
         public DateTime Date { get; set; }
         public IEnumerable<SubmitedAnswer> SubmitedAnswers { get; set; }
