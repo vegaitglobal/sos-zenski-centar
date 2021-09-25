9459557517af9012c718968afc70ed16e36cd3e8
Srdjan Stankov <s.stankov@vegait.rs>
2021-09-25 13:14:05 +0200

    Create Users

---
+++ seed-data.sql
@@ -0,0 +1,279 @@
+﻿INSERT INTO Users (
+    first_name,
+    last_name,
+    email,
+    password
+)
+VALUES
+    
+	(
+		'vanjastepanovic001@gmail.com',
+		'Vanja',
+		'Stepanović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'noapernoa@gmail.com',
+		'Radojka',
+		'Jevtić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'vtesan95@gmail.com',
+		'Ivana',
+		'Tešan',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'vanjatvanja@gmail.com',
+		'Vanja',
+		'Trkulja',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'maja.pistolic@gmail.com',
+		'Maja',
+		'Pistolić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'jovana.kostevski@gmail.com',
+		'Jovana',
+		'Kostevski',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'ivana.nesovic@sosns.rs',
+		'Ivana',
+		'Nešović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'teodora.tepic@sosns.rs',
+		'Teodora',
+		'Tepić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'jovanaradulovic22@gmail.com',
+		'Jovana',
+		'Radulović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'andri.kosanin@gmail.com',
+		'Andrea',
+		'Košanin',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'jelenastojanovic@sosns.rs',
+		'Jelena',
+		'Stojanović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'cupcake.nin@gmail.com',
+		'Nina',
+		'Racić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'saska.milovac795@gmail.com',
+		'Aleksandra',
+		'Dukić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'oliverakovacev@yahoo.com ',
+		'Olivera',
+		'Kovačev',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'sos.telefon@gmail.com',
+		'Ivana',
+		'Perić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'masapoznanovic@sosns.rs',
+		'Maša',
+		'Poznanović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'biljanagalic@sosns.rs',
+		'Biljana',
+		'Galić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'psihologanakusturic@gmail.com',
+		'Ana',
+		'Todorčević',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'dusicasostaric@yahoo.com',
+		'Dušica',
+		'Šoštarić Blagojević',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'mirela.pavlovic@gmail.com',
+		'Mirela',
+		'Pavlović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'nedasubotic21@gmail.com',
+		'Neda',
+		'Subotić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'aleksandracurcic16@gmail.com ',
+		'Aleksandra',
+		'Ćurčić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'jovana.ozimkovic@gmail.com',
+		'Jovana',
+		'Ozimković',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'pejicsilvana@gmail.com',
+		'Silvana',
+		'Pejić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'sandrafilipovic998@gmail.com ',
+		'Sandra',
+		'Filipović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'saska.bajic86@gmail.com ',
+		'Aleksandra',
+		'Bajić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'tanja.psychology@gmail.com',
+		'Tanja',
+		'Adamović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'zlatkovicaleksandra2@gmail.com',
+		'Aleksandra',
+		'Zlatković',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'milenapaunkovic@hotmail.com',
+		'Milena',
+		'Paunković',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'milica.ludoski95@gmail.com',
+		'Milica',
+		'Ludoški',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'zorana.damjanovic.savetovanje@gmail.com ',
+		'Zorana',
+		'Damjanović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'mdespotovic996@gmail.com',
+		'Maja',
+		'Despotović',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'nedasubotic21@gmail.com ',
+		'Neda',
+		'Subotić ',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	),
+	
+	(
+		'nszorica@gmail.com',
+		'Zorica',
+		'Pajantić',
+		'JMt8k8MmtBSW1KY7uCtALWl2Cg0='
+		
+	)
--- sos-centar-api/SosCentar.BusinessLogic/Services/SecurityService.cs
+++ sos-centar-api/SosCentar.BusinessLogic/Services/SecurityService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
-using SosCentar.Contracts.Interfaces.Repositories;
 using SosCentar.Contracts.Interfaces.Services;
 using System;
 using System.Collections.Generic;
@@ -15,13 +14,11 @@ namespace SosCentar.BusinessLogic.Services
     {
         private readonly SymmetricSecurityKey _key;
         private readonly byte[] _salt;
-        private readonly IUserRepository _userRepository;
 
-        public SecurityService(IConfiguration configuration, IUserRepository userRepository)
+        public SecurityService(IConfiguration configuration)
         {
             _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["TokenKey"]));
             _salt = Encoding.UTF8.GetBytes(configuration["Salt"]);
-            _userRepository = userRepository;
         }
 
         public string CreateToken(string userEmail)
@@ -52,11 +49,6 @@ namespace SosCentar.BusinessLogic.Services
             var pbkdf2 = new Rfc2898DeriveBytes(password, _salt, 10000);
 
             return Convert.ToBase64String(pbkdf2.GetBytes(20));
-        }
-
-        public bool ValidateUserCredentials(string email, string password)
-        {
-            return _userRepository.ValidateUserExists(email, HashPassword(password));
         }
     }
 }
