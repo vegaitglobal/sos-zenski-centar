fe4cf3f9da529ab414ae2dd0f6b3143076841d5e
Igor Cernjak <i.cernjak@vegait.rs>
2021-09-25 08:47:00 +0200

    Initial setup for Login page

--- sos-web/src/routes/Routes.js
+++ sos-web/src/routes/Routes.js
@@ -1,5 +1,6 @@
 import { Route, Switch } from 'react-router-dom';
 import { Home } from '../components/pages/Home';
+import { Login } from '../components/pages/Login';
 import { Reports } from '../components/pages/Reports';
 
 export const Routes = () => (
@@ -9,6 +10,9 @@ export const Routes = () => (
     </Route>
     <Route path="/reports" exact>
       <Reports />
+    </Route>
+    <Route path="/login" exact>
+      <Login />
     </Route>
   </Switch>
 );
