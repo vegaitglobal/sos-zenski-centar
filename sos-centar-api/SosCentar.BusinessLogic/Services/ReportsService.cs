6ec56a0b9cb4d71ba0c9cd25a501048d6c8c166d
Uros Vukovic <u.vukovic@vegait.rs>
2021-09-26 12:19:21 +0200

    Frist graph endpoint

--- sos-centar-api/SosCentar.BusinessLogic/Services/ReportsService.cs
+++ sos-centar-api/SosCentar.BusinessLogic/Services/ReportsService.cs
@@ -19,38 +19,46 @@ namespace SosCentar.BusinessLogic.Services
 		}
 
 		public IEnumerable<GraphDto> GetGraphs(DateTime From, DateTime To)
-		{
-			var Cache = new Dictionary<string, int>();
-			var Categories = _categoryService.GetAll();
-			var Graph = new GraphDto();
-			Graph.Label = "Broj korisnika/ca po uslugama";
-			foreach (var Category in Categories)
-			{
-				Cache.Add(Category.Label, 0);
+        {
+            List<GraphDto> retList;
+            var FirstGraph = _getFirstGraph(From, To);
+            var RetList = new List<GraphDto>();
+            RetList.Add(FirstGraph);
+            return RetList;
+        }
+
+        private GraphDto _getFirstGraph(DateTime From, DateTime To)
+        {
+            var Cache = new Dictionary<string, int>();
+            GraphDto Graph;
+            var Categories = _categoryService.GetAll();
+            Graph = new GraphDto();
+            Graph.Label = "Broj korisnika/ca po uslugama";
+            foreach (var Category in Categories)
+            {
+                Cache.Add(Category.Label, 0);
             }
 
-			var Entries = _entryService.GetInRange(From, To);
-			foreach (var Entry in Entries)
+            var Entries = _entryService.GetInRange(From, To);
+            foreach (var Entry in Entries)
             {
-				var OldCount = Cache[Entry.Category.Name];
-				var NewCount = OldCount + 1;
-				Cache[Entry.Category.Name] = NewCount;
-			}
-			var Data = new List<GraphSliceDto>();
-			foreach (var Item in Cache)
-            {
-				var Dto = new GraphSliceDto();
-				Dto.Label = Item.Key;
-				Dto.Level = Item.Value.ToString();
-				Data.Add(Dto);
+                var OldCount = Cache[Entry.Category.Name];
+                var NewCount = OldCount + 1;
+                Cache[Entry.Category.Name] = NewCount;
             }
-			Graph.Data = Data;
-			var retList = new List<GraphDto>();
-			retList.Add(Graph);
-			return retList;
-		}
+            var Data = new List<GraphSliceDto>();
+            foreach (var Item in Cache)
+            {
+                var Dto = new GraphSliceDto();
+                Dto.Label = Item.Key;
+                Dto.Level = Item.Value.ToString();
+                Data.Add(Dto);
+            }
+            Graph.Data = Data;
+            return Graph;
+        }
 
-		public Table GetTableReport()
+        public Table GetTableReport()
 		{
 			throw new System.NotImplementedException();
 		}
