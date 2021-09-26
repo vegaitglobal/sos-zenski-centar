14abd01642b218be09ed1ec02282d42fa9c19b61
Sasa Stevcic <s.stevcic@vegait.rs>
2021-09-26 15:21:06 +0200

    Add format date util

--- sos-web/src/components/organisms/ReportsContent/DownloadReportContent.js
+++ sos-web/src/components/organisms/ReportsContent/DownloadReportContent.js
@@ -1,25 +1,72 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { BASE_URL, useFetch } from '../../../hooks/useFetch';
+import { getLastMonth } from '../../../utils/date';
+import { getToken } from '../../../utils/isAuthenticated';
+import { useDataContext } from '../../../utils/store';
 import { DatePicker } from '../../atoms/DatePicker/DatePicker';
 import { Button } from '../../molecules/Button/Button';
 import { StyledGrid } from './DownloadReportContent.styles';
 
+const headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+  Authorization: `Bearer ${getToken()}`,
+};
+
 export const DownloadReport = () => {
+  const { setData } = useDataContext();
+  const { sendRequest } = useFetch();
   const [date, setDate] = useState({
     start: '',
     end: '',
   });
 
-  const handleOnChange = ({ target }) => {
+  const fetchGraphs = useCallback(
+    async (start = date.start, end = date.end) => {
+      const { firstDay, lastDay } = getLastMonth();
+
+      const response = await sendRequest(
+        `${BASE_URL}/ReportGraphs?from=${start || firstDay}&to=${
+          end || lastDay
+        }`,
+        'GET',
+        headers,
+      );
+
+      setData({
+        barChart: response,
+      });
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [sendRequest, date.start, date.end],
+  );
+
+  const handleOnChange = useCallback(({ target }) => {
     setDate((previousState) => ({
       ...previousState,
       [target.name]: target.value,
     }));
-  };
+  }, []);
+
+  const handleCustomDate = useCallback(() => {
+    fetchGraphs();
+  }, [fetchGraphs]);
+
+  const handleLastMonth = useCallback(() => {
+    const { firstDay, lastDay } = getLastMonth();
+
+    fetchGraphs(firstDay, lastDay);
+  }, [fetchGraphs]);
+
+  useEffect(() => {
+    fetchGraphs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <StyledGrid>
       <div>
-        <Button>Prošli mesec</Button>
+        <Button onClick={handleLastMonth}>Prošli mesec</Button>
       </div>
       <div>
         <DatePicker
@@ -34,7 +81,7 @@ export const DownloadReport = () => {
           value={date.end}
           onChange={handleOnChange}
         />
-        <Button>Izaberi</Button>
+        <Button onClick={handleCustomDate}>Izaberi</Button>
       </div>
       <div>
         <Button>Download</Button>
--- sos-web/src/components/organisms/ReportsContent/ReportsContent.js
+++ sos-web/src/components/organisms/ReportsContent/ReportsContent.js
@@ -8,131 +8,134 @@ import { DoughnutChart } from '../DoughnutChart/DoughnutChart';
 import { Shell } from '../Shell/Shell';
 import { StyledGrid } from './ReportsContent.styles';
 import { DownloadReport } from './DownloadReportContent';
+import { BASE_URL, useFetch } from '../../../hooks/useFetch';
+import { useEffect } from 'react';
+import { getToken } from '../../../utils/isAuthenticated';
+import { useDataContext } from '../../../utils/store';
 
+const table1Title =
+  '3.1. Broj klijentkinja i klijenata sa iskustvom nasilja po uslugama';
+
+const table2Title = '3.2. Odnos žrtve sa nasilnikom';
+
+const array1 = [
+  {
+    text: 'SOS telefon',
+    value: 38,
+  },
+  {
+    text: 'Poruke',
+    value: 12,
+  },
+  {
+    text: 'Psihološko savetovanje',
+    value: 14,
+  },
+  {
+    text: 'Omladinsko savetovalište',
+    value: 8,
+  },
+  {
+    text: 'Pravna pomoć',
+    value: 45,
+  },
+  {
+    text: 'Ukupno',
+    value: 117,
+  },
+];
+
+const array2 = [
+  {
+    text: 'Vanbračni partner',
+    frequence: 11,
+    value: 6.03,
+  },
+  {
+    text: 'Bračni partner',
+    frequence: 38,
+    value: 21.08,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+  {
+    text: 'Bivši vanbračni partner',
+    frequence: 34,
+    value: 19.5,
+  },
+];
+
+const table1 = [
+  {
+    text: 'Kategorija',
+    value: 'text',
+  },
+  {
+    text: 'Broj klijenata/kinja',
+    value: 'value',
+  },
+];
+
+const table2 = [
+  {
+    text: 'Rodbinska veza',
+    value: 'text',
+  },
+  {
+    text: 'Frekvencija',
+    value: 'value',
+  },
+  {
+    text: 'Procenti',
+    value: 'value',
+  },
+];
 export const ReportsContent = () => {
-  const table1Title =
-    '3.1. Broj klijentkinja i klijenata sa iskustvom nasilja po uslugama';
-
-  const table2Title = '3.2. Odnos žrtve sa nasilnikom';
-
-  const array1 = [
-    {
-      text: 'SOS telefon',
-      value: 38,
-    },
-    {
-      text: 'Poruke',
-      value: 12,
-    },
-    {
-      text: 'Psihološko savetovanje',
-      value: 14,
-    },
-    {
-      text: 'Omladinsko savetovalište',
-      value: 8,
-    },
-    {
-      text: 'Pravna pomoć',
-      value: 45,
-    },
-    {
-      text: 'Ukupno',
-      value: 117,
-    },
-  ];
-
-  const array2 = [
-    {
-      text: 'Vanbračni partner',
-      frequence: 11,
-      value: 6.03,
-    },
-    {
-      text: 'Bračni partner',
-      frequence: 38,
-      value: 21.08,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-    {
-      text: 'Bivši vanbračni partner',
-      frequence: 34,
-      value: 19.5,
-    },
-  ];
-
-  const table1 = [
-    {
-      text: 'Kategorija',
-      value: 'text',
-    },
-    {
-      text: 'Broj klijenata/kinja',
-      value: 'value',
-    },
-  ];
-
-  const table2 = [
-    {
-      text: 'Rodbinska veza',
-      value: 'text',
-    },
-    {
-      text: 'Frekvencija',
-      value: 'value',
-    },
-    {
-      text: 'Procenti',
-      value: 'value',
-    },
-  ];
-
   return (
     <Shell
       backgroundColor={rgba(theme.color.greyLighter, 0.3)}
       title="Izveštaj"
     >
-      <Accordion title="1. Vremenski period">
-        <DownloadReport></DownloadReport>
+      <Accordion title="1. Vremenski period" defaultOpened>
+        <DownloadReport />
       </Accordion>
       <Accordion title="2. Opšti podaci" defaultOpened>
         <StyledGrid>
