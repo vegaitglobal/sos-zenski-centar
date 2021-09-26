c1c056ecf4f4b04fdd6a041795618865c511d430
Uros Vukovic <u.vukovic@vegait.rs>
2021-09-26 17:25:40 +0200

    Include in get gorm base

--- sos-centar-api/SosCentar.DataAccess/Repositories/QuestionRepository.cs
+++ sos-centar-api/SosCentar.DataAccess/Repositories/QuestionRepository.cs
@@ -1,4 +1,5 @@
-﻿using SosCentar.Contracts.Interfaces.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+using SosCentar.Contracts.Interfaces.Repositories;
 using SosCentar.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -6,23 +7,23 @@ using System.Linq;
 
 namespace SosCentar.DataAccess.Repositories
 {
-	public class QuestionRepository : IQuestionRepository
-	{
+    public class QuestionRepository : IQuestionRepository
+    {
         private readonly ReportContext _reportContext;
 
-		public QuestionRepository(ReportContext reportContext)
-		{
-			_reportContext = reportContext;
-		}
+        public QuestionRepository(ReportContext reportContext)
+        {
+            _reportContext = reportContext;
+        }
 
-		public Question GetById(Guid id)
-		{
-			return _reportContext.Questions.FirstOrDefault(question => question.Id == id);
-		}
+        public Question GetById(Guid id)
+        {
+            return _reportContext.Questions.FirstOrDefault(question => question.Id == id);
+        }
 
-		public IEnumerable<Question> GetByName(string name)
-		{
-			return _reportContext.Questions.Where(question => question.Text == name);
-		}
-	}
+        public IEnumerable<Question> GetByName(string name)
+        {
+            return _reportContext.Questions.Include(question => question.Answers).Where(question => question.Text == name);
+        }
+    }
 }
