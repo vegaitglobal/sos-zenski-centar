7c7e4881a87af8aec1154000d416a13c5efab22e
Vuk Vlahovic <v.vlahovic@vega.local>
2021-09-26 03:32:43 +0200

    questions finished

--- sos-centar-api/data/seed-questions.sql
+++ sos-centar-api/data/seed-questions.sql
@@ -319,4 +319,884 @@
 		'5',
 		'1',
 		'64f02514-cac6-4563-b1b7-954743c7c05f'
+	),
+	(
+		'426342d5-cc6b-4b9d-b341-57c241a8d283',
+		'Da li je klijent završio besplatne susrete?',
+		'1',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'6e87d97f-c5df-47ee-9a5b-27a664c7f037',
+		'Da li je klijent završio besplatne susrete?',
+		'2',
+		'1',
+		'cb428035-1089-4026-a39b-925560ab6615'
+	),
+	(
+		'2c863685-190e-437e-91f4-6560275e3ee3',
+		'Da li je klijent završio besplatne susrete?',
+		'3',
+		'1',
+		'0da678bd-9110-4dad-97cf-a531ceef1622'
+	),
+	(
+		'546bd8e9-9ac4-4a90-8a59-195ef6e426aa',
+		'Da li je klijent završio besplatne susrete?',
+		'4',
+		'1',
+		'7875c455-6ac9-4daa-a75b-761b0845449c'
+	),
+	(
+		'4c715135-574d-4194-8396-4cd51cd30ba6',
+		'Da li je klijent završio besplatne susrete?',
+		'5',
+		'1',
+		'64f02514-cac6-4563-b1b7-954743c7c05f'
+	),
+	(
+		'cb323c5d-a83d-4135-957c-4c009be99377',
+		'Kako je klijent saznao za psihološko savetovalište?',
+		'1',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'ab1baaa1-cb5d-4c91-aaed-9a0cf56afedc',
+		'Kako je klijent saznao za psihološko savetovalište?',
+		'2',
+		'1',
+		'cb428035-1089-4026-a39b-925560ab6615'
+	),
+	(
+		'f34d6a23-fb51-41df-a2b6-9a6f7a78ffdb',
+		'Kako je klijent saznao za psihološko savetovalište?',
+		'3',
+		'1',
+		'0da678bd-9110-4dad-97cf-a531ceef1622'
+	),
+	(
+		'8d690cff-f64d-4ce9-987b-3adae660d9f0',
+		'Kako je klijent saznao za psihološko savetovalište?',
+		'4',
+		'1',
+		'7875c455-6ac9-4daa-a75b-761b0845449c'
+	),
+	(
+		'0fb3acbf-5fff-46ae-96e4-da555bf73022',
+		'Kako je klijent saznao za psihološko savetovalište?',
+		'5',
+		'1',
+		'64f02514-cac6-4563-b1b7-954743c7c05f'
+	),
+	(
+		'2f55d024-cac5-4428-8d25-b9d933bd944f',
+		'Broj susreta',
+		'1',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'e1f17fb1-b8fa-4c4d-b61c-1978354b537f',
+		'Broj susreta',
+		'2',
+		'1',
+		'cb428035-1089-4026-a39b-925560ab6615'
+	),
+	(
+		'bb7e48cf-19bf-4fce-9e2a-5e45d99fd837',
+		'Broj susreta',
+		'3',
+		'1',
+		'0da678bd-9110-4dad-97cf-a531ceef1622'
+	),
+	(
+		'd70a6dc9-c454-4990-b6d5-3ccfd13280c5',
+		'Broj susreta',
+		'4',
+		'1',
+		'7875c455-6ac9-4daa-a75b-761b0845449c'
+	),
+	(
+		'ee0439c5-f3d4-431e-b6d8-54ddeda01ccf',
+		'Broj susreta',
+		'5',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'83b1379a-8fb5-4912-b12a-146cee6e0b07',
+		'Da li je bilo suđenja?',
+		'1',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'51084ca7-4e77-41c9-a444-21de52a24706',
+		'Da li je bilo suđenja?',
+		'2',
+		'1',
+		'cb428035-1089-4026-a39b-925560ab6615'
+	),
+	(
+		'dc651e77-4f78-43f5-92c8-a55e374b161e',
+		'Da li je bilo suđenja?',
+		'3',
+		'1',
+		'0da678bd-9110-4dad-97cf-a531ceef1622'
+	),
+	(
+		'69ecd876-c396-49f1-b870-8e48fc1b5477',
+		'Da li je bilo suđenja?',
+		'4',
+		'1',
+		'7875c455-6ac9-4daa-a75b-761b0845449c'
+	),
+	(
+		'074951d2-9ed0-41bc-98e3-260256c7fae9',
+		'Da li je bilo suđenja?',
+		'5',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	-----------------------------------------------CONDITIONAL DA LI JE BILO SUDJENJA
+	(
+		'56017f27-5b1c-403c-9d56-81d4a46d991f',
+		'Alimentacija',
+		'1',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	(
+		'f4dcc980-970d-4f68-8e04-6b04b8d70c52',
+		'Alimentacija',
+		'2',
+		'1',
+		'cb428035-1089-4026-a39b-925560ab6615'
+	),
+	(
+		'14fc2fb6-8bee-4b75-ab23-ff4d05270794',
+		'Alimentacija',
+		'3',
+		'1',
+		'0da678bd-9110-4dad-97cf-a531ceef1622'
+	),
+	(
+		'4084f3ad-4310-48f2-9090-c0375bc0518c',
+		'Alimentacija',
+		'4',
+		'1',
+		'7875c455-6ac9-4daa-a75b-761b0845449c'
+	),
+	(
+		'57029a5a-a9e3-421b-a805-d44907f26ce8',
+		'Alimentacija',
+		'5',
+		'1',
+		'fdc12b45-2d12-48b5-b940-4ed7cf78e2b2'
+	),
+	---------------------------SEKCIJA POZIV--------------------------
+	(
+		'4d9e7553-9a57-4f58-bedc-de98bb21b60b',
+		'Pol',
+		'1',
+		'0',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'14413365-b2d1-4551-93d7-a17fb0317068',
+		'Pol',
+		'2',
+		'0',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'2b47896c-722e-488b-b611-2ddc408f0275',
+		'Pol',
+		'3',
+		'0',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'540c2a5f-13d6-4464-ac7c-0c9b94ae9a94',
+		'Pol',
+		'4',
+		'0',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'45f57722-2227-45c3-a234-2d0435f82e43',
+		'Pol',
+		'5',
+		'0',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'7fe3dc15-0021-4dbf-9833-0044a60195b6',
+		'Mesto',
+		'1',
+		'0',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'05841799-a62d-4df6-88fc-fd1b40ff8a64',
+		'Mesto',
+		'2',
+		'0',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'e8dd5afc-1d34-41ad-8dbd-50f50f8228b7',
+		'Mesto',
+		'3',
+		'0',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'ce8dd0c9-e508-4cff-bb81-612023a604ad',
+		'Mesto',
+		'4',
+		'0',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'1ec3dc37-7310-4848-a9e1-29c394688b41',
+		'Mesto',
+		'5',
+		'0',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'3f661a72-9f51-42ea-aeac-a2af94ec90b0',
+		'Uzrast',
+		'1',
+		'0',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'a3d0c8b4-940b-4322-ab2d-dcf8fe72ab58',
+		'Uzrast',
+		'2',
+		'0',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'fe042d64-8af5-4819-a8af-47b157322d80',
+		'Uzrast',
+		'3',
+		'0',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'c9d310c2-922e-4f9d-ae77-930d454c877d',
+		'Uzrast',
+		'4',
+		'0',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'35a10398-874e-4aa7-874c-ef05bca8eaf2',
+		'Uzrast',
+		'5',
+		'0',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'5e8a76cb-0df2-4a69-a580-6890b2eeec44',
+		'Radni status',
+		'1',
+		'0',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'3b5f268a-3f6c-47cb-b9f3-e6d080d1b144',
+		'Radni status',
+		'2',
+		'0',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'93e806e9-5ca7-4b49-810e-9d3483a9a28a',
+		'Radni status',
+		'3',
+		'0',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'4380a5ac-34ba-480a-b214-2b4de9e73ba5',
+		'Radni status',
+		'4',
+		'0',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'116897e7-60c9-4ecf-befe-0d7c8201eb57',
+		'Radni status',
+		'5',
+		'0',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'89abc9af-6a2b-4494-b2c1-de24697e338f',
+		'Da li postoji nasilje?',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'5a8e6579-2aee-4dc6-8e2e-545243eef067',
+		'Da li postoji nasilje?',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'503396ff-b024-48e8-bb5b-0d1aea23299a',
+		'Da li postoji nasilje?',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'3417448f-052d-48d7-aa2e-a6a5330f5da7',
+		'Da li postoji nasilje?',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'9d7900cf-c4b0-45fc-8a31-8dcff39f6aa2',
+		'Da li postoji nasilje?',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'76646393-c582-4cc5-8e94-8fc842ac8109',
+		'Bračni status',
+		'1',
+		'0',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'a73e4914-e9c2-446e-8cfc-3281f91c6545',
+		'Bračni status',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'eb73e60a-2716-4cad-b394-df0a130698a7',
+		'Bračni status',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'79482883-908b-44b7-aedb-35295b43541f',
+		'Bračni status',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'ab557663-91b3-4849-9544-808fc4cb2cb1',
+		'Bračni status',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'1d767c87-7ab5-492d-bf2f-6dc7a9a7d038',
+		'Stambena situacija',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'80dde988-a7ec-4663-b752-c5e7c6eeedd8',
+		'Stambena situacija',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'616d17c9-c208-436c-8ae4-a7d69a80b909',
+		'Stambena situacija',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'8ee5c401-75bd-4e1d-92fd-4af8a1d684ee',
+		'Stambena situacija',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'b32de8c0-8959-4588-b4e0-675f58d10a4d',
+		'Stambena situacija',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'1d1a4ab5-79a9-44b7-a9a6-00d4da667d58',
+		'Pripadnost marginalitovanim grupama',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'b8ee36c3-bcad-4b5a-b387-236d2dc40e2b',
+		'Pripadnost marginalitovanim grupama',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'406c8020-6f73-4836-b261-e6a10e1f2144',
+		'Pripadnost marginalitovanim grupama',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'81fabbd7-1381-4c08-946a-bc4ba22d391b',
+		'Pripadnost marginalitovanim grupama',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'52a7bf4b-277e-4f42-aa71-e0d7e7fee47c',
+		'Pripadnost marginalitovanim grupama',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	---------------------------------KONDICIONAL DA LI POSTOJI NASILJE
+	(
+		'ba5d614d-8394-4d0b-904c-4799ee7580d4',
+		'Ko trpi nasilje?',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'ae73e2f1-fee8-4676-b252-bfe9be13a3e9',
+		'Ko trpi nasilje?',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'91c75273-e9ff-453d-8be1-e5f4ae80fc89',
+		'Ko trpi nasilje?',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'369f6ca0-e286-4dcd-9d29-55fccd3b1cd2',
+		'Ko trpi nasilje?',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'563e0b61-a469-4697-8028-7d52e880e35b',
+		'Ko trpi nasilje?',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	--------------------UZRAST DETETA KONDICIONAL
+	(
+		'a1398937-e4db-44a9-a386-b242d56202e5',
+		'Uzrast deteta',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'a35eb4c1-9fec-485b-ad8c-334d5dfe2ebd',
+		'Uzrast deteta',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'5eaeb138-f2f8-4b3b-94ba-506e254fd2d9',
+		'Uzrast deteta',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'9f083f8a-9c68-484c-9d39-ad093d697800',
+		'Uzrast deteta',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'ea5150b3-1a64-400b-9b89-2305d6e53a99',
+		'Uzrast deteta',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'e4542713-db23-4077-925c-dea28dccd9fb',
+		'Odnos sa nasilnikom',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'de476f53-1292-4bed-acfb-d38aedce271f',
+		'Odnos sa nasilnikom',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'4fdb3f0c-5f7c-46e4-9e35-075791dac07a',
+		'Odnos sa nasilnikom',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'0f5e78b3-f3af-4c34-8f35-df68231800b7',
+		'Odnos sa nasilnikom',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'9fa0185b-9dc6-445c-aae6-ea54eda7dc0a',
+		'Odnos sa nasilnikom',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'657a5fd3-d9a9-4d61-a392-a54caefd4bb6',
+		'Prisutni oblici nasilja',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'146a9993-70df-4e2c-8cad-4e1c3aee1377',
+		'Prisutni oblici nasilja',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'0c465680-db66-428e-ac6b-6a1754e178ef',
+		'Prisutni oblici nasilja',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'cf6c7d5e-0c99-41e0-aa26-113ee257b70f',
+		'Prisutni oblici nasilja',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'56cb349a-b7bd-4825-9e29-6ffd6c383ab8',
+		'Prisutni oblici nasilja',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'33d905b0-fe1a-4461-9ff0-8aa3fd3cf633',
+		'Pol nasilnika',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'97239ec4-941b-4d26-b5e1-7bc32839bc1e',
+		'Pol nasilnika',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'747820f8-13b7-42a5-8379-5a5761005527',
+		'Pol nasilnika',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'3d3f6efd-ba2c-45ae-b109-69993327e34e',
+		'Pol nasilnika',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'899b839c-2175-435e-a8bc-b3f57376ff8b',
+		'Pol nasilnika',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'70c32d0f-e1ef-46ac-b1c6-78b5d0b10411',
+		'Nasilnik je ranije prijavljen za nasilje?',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'27acb1ae-5ebd-494e-bdcc-f2e5306812c0',
+		'Nasilnik je ranije prijavljen za nasilje?',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'f2379527-e080-41e4-afc5-506c21fa4406',
+		'Nasilnik je ranije prijavljen za nasilje?',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'ed00c8de-d334-4d00-bc51-180038a955fb',
+		'Nasilnik je ranije prijavljen za nasilje?',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'415df83e-685d-4da7-bc56-ab2955fd099b',
+		'Nasilnik je ranije prijavljen za nasilje?',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'326cf6c3-cbd1-4fed-bb78-e8a58e963099',
+		'Nasilnik ima izrečene hitne mere?',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'7ec7607f-9a06-4909-9eef-2be475ab7a9b',
+		'Nasilnik ima izrečene hitne mere?',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'1c0cb9d5-43a9-4318-ab2f-196579398a2c',
+		'Nasilnik ima izrečene hitne mere?',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'b6e8ca52-433a-49e0-aac2-6e4ddce3e040',
+		'Nasilnik ima izrečene hitne mere?',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'046a6ede-7845-411b-b444-11e537aa1320',
+		'Nasilnik ima izrečene hitne mere?',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	(
+		'69b1fc08-1609-4000-966b-0a750b4bf3d4',
+		'Nasilnik ima krivično delo nasilja u porodici?',
+		'1',
+		'1',
+		'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	),
+	(
+		'0cbb6b00-018e-4a1d-8f50-cb54c459207c',
+		'Nasilnik ima krivično delo nasilja u porodici?',
+		'2',
+		'1',
+		'393de121-fcff-4932-bb46-606e4509a302'
+	),
+	(
+		'a62dd43d-0a37-4461-b6b1-aedbb12b41ed',
+		'Nasilnik ima krivično delo nasilja u porodici?',
+		'3',
+		'1',
+		'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	),
+	(
+		'e480a779-f47e-4145-8a48-b926d3f6adee',
+		'Nasilnik ima krivično delo nasilja u porodici?',
+		'4',
+		'1',
+		'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	),
+	(
+		'bed1f5ee-51ff-43a6-a310-1a2c44b79e61',
+		'Nasilnik ima krivično delo nasilja u porodici?',
+		'5',
+		'1',
+		'a8e718f6-6832-4e57-b387-3349a99cce32'
+	),
+	-----------------------------------------------------INTERVENCIJE SEKCIJA-----------------------------
+	(
+		'01cb293b-d67c-4204-a92a-85dfee4b13f5',
+		'Korisnica je dalje upućena:',
+		'1',
+		'1',
+		'8e445aa1-4587-4433-824b-f0ff900f840e'
+	),
+	(
+		'01498b24-ce6d-4c40-aff6-1afdbb159e29',
+		'Korisnica je dalje upućena:',
+		'2',
+		'1',
+		'40fdd452-3837-40e5-a1d5-6a6f7774f838'
+	),
+	(
+		'fe6e0bb8-932e-48a5-8efc-f2121bfdc11b',
+		'Korisnica je dalje upućena:',
+		'3',
+		'1',
+		'8f72b1e0-7a29-4659-b1a1-89c247e96392'
+	),
+	(
+		'19e0d305-adbc-4c90-8153-ae1c0dd86407',
+		'Korisnica je dalje upućena:',
+		'4',
+		'1',
+		'7d59dc7a-5d64-4210-8446-53c885b945cc'
+	),
+	(
+		'271760b4-c7df-4892-9c85-5ed61eb9e555',
+		'Korisnica je dalje upućena:',
+		'5',
+		'1',
+		'08052000-470a-4bf9-8809-b1bf7b45d528'
+	),
+	(
+		'97ce0828-199d-4903-a1a8-40ea0243e0f3',
+		'Urađena je procena rizika',
+		'1',
+		'1',
+		'8e445aa1-4587-4433-824b-f0ff900f840e'
+	),
+	(
+		'd2a7b5c8-e5af-4c30-95b0-a5cd917f36ca',
+		'Urađena je procena rizika',
+		'2',
+		'1',
+		'40fdd452-3837-40e5-a1d5-6a6f7774f838'
+	),
+	(
+		'7e7e426d-9bce-4632-8756-ebc87f444668',
+		'Urađena je procena rizika',
+		'3',
+		'1',
+		'8f72b1e0-7a29-4659-b1a1-89c247e96392'
+	),
+	(
+		'f0dc617e-299e-4086-b7cf-fe8cae64c10b',
+		'Urađena je procena rizika',
+		'4',
+		'1',
+		'7d59dc7a-5d64-4210-8446-53c885b945cc'
+	),
+	(
+		'c6ed2f65-e439-4d2b-a876-f3d4712818aa',
+		'Urađena je procena rizika',
+		'5',
+		'1',
+		'08052000-470a-4bf9-8809-b1bf7b45d528'
+	),
+	(
+		'dca28ae5-f566-4833-bd41-e3201e342f26',
+		'Urađen je bezbednosni plan',
+		'1',
+		'1',
+		'8e445aa1-4587-4433-824b-f0ff900f840e'
+	),
+	(
+		'dc8e3eb0-98f5-42a3-bfba-34a3504570e6',
+		'Urađen je bezbednosni plan',
+		'2',
+		'1',
+		'40fdd452-3837-40e5-a1d5-6a6f7774f838'
+	),
+	(
+		'e7c7122c-775c-46b7-96fa-969846a63329',
+		'Urađen je bezbednosni plan',
+		'3',
+		'1',
+		'8f72b1e0-7a29-4659-b1a1-89c247e96392'
+	),
+	(
+		'ab952186-ebef-49a8-b2cf-2534ea93a328',
+		'Urađen je bezbednosni plan',
+		'4',
+		'1',
+		'7d59dc7a-5d64-4210-8446-53c885b945cc'
+	),
+	(
+		'6774af12-9b13-47eb-80dd-eb2af3197f9d',
+		'Urađen je bezbednosni plan',
+		'5',
+		'1',
+		'08052000-470a-4bf9-8809-b1bf7b45d528'
+	),
+	(
+		'a9578557-846b-43dd-8f1a-41a3d8c504c7',
+		'Pravne intervencije',
+		'1',
+		'1',
+		'8e445aa1-4587-4433-824b-f0ff900f840e'
+	),
+	(
+		'fd60fdc7-b4c6-43c0-8199-db96871eb322',
+		'Pravne intervencije',
+		'2',
+		'1',
+		'40fdd452-3837-40e5-a1d5-6a6f7774f838'
+	),
+	(
+		'1acd10b9-dca4-424f-9dfd-25f98cee6ce2',
+		'Pravne intervencije',
+		'3',
+		'1',
+		'8f72b1e0-7a29-4659-b1a1-89c247e96392'
+	),
+	(
+		'130e83db-83cd-4f22-a52b-a23e1ba5346a',
+		'Pravne intervencije',
+		'4',
+		'1',
+		'7d59dc7a-5d64-4210-8446-53c885b945cc'
+	),
+	(
+		'54e9af82-6c54-4851-ac00-9ee68d05abac',
+		'Pravne intervencije',
+		'5',
+		'1',
+		'08052000-470a-4bf9-8809-b1bf7b45d528'
 	);
