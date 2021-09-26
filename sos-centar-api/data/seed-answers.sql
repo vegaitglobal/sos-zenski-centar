a7d6b7308da55e57160d1bfbdb3e1515c2179f3c
Vuk Vlahovic <v.vlahovic@vega.local>
2021-09-26 11:57:34 +0200

    Update data

---
+++ sos-centar-api/data/seed-answers.sql
@@ -0,0 +1,37 @@
+INSERT INTO "Answers" (
+	"Id",
+	"Text",
+    "Order",
+    "QuestionId"
+) 
+VALUES
+    (
+        "f63243a3-ed41-4c27-998d-15675b0e25d7",
+        "Prvi put",
+        "1",
+        "ee3b6d28-de62-4a60-8894-56b1650a0388"
+    ),
+    (
+        "6cf4b54a-04a1-4198-aed8-100a1c69cb2c",
+        "Prvi put",
+        "1",
+        "b188cc90-2ee8-4762-a7c8-7da452ecf1cb"
+    ),
+    (
+        "e82d3a78-a701-4c39-9ea0-b3f34d7b96cf",
+        "Prvi put",
+        "1",
+        "6f1e681f-df49-4f3c-b716-7ebca52534a5"
+    ),
+    (
+        "3b76d528-ee34-4988-a208-1716305c75c1",
+        "Prvi put",
+        "1",
+        "993a4e22-69d6-4867-b121-cc0b7b2c3851"
+    ),
+    (
+        "45ecce13-6a72-4d62-8904-585fc26a9eda",
+        "Prvi put",
+        "1",
+        "b188cc90-2ee8-4762-a7c8-7da452ecf1cb"
+    ),
