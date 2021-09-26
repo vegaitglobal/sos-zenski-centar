6fc66b6cd0b303794520a0363798984c527cbbf1
n.porobic@vegait.rs <n.porobic@vegait.rs>
2021-09-26 16:31:32 +0200

    fix issue on seed-answers-nasilje

--- sos-centar-api/data/seed-answers-nasilje.sql
+++ sos-centar-api/data/seed-answers-nasilje.sql
@@ -1,8 +1,8 @@
 INSERT INTO "Answers" (
-	"Id",
-	"Text",
-  "Order",
-  "QuestionId"
+"Id",
+"Text",
+"Order",
+"QuestionId"
 ) VALUES
 ----------------------- Ko trpi nasilje --------------------------------------
 -------------------------------Prisutni oblici nasilja----------------------------------
@@ -10,483 +10,503 @@ INSERT INTO "Answers" (
 	'f5b8d2fa-2cf1-4113-80bf-11d380fccd38',
 	'Psihičko nasilje',
 	'52',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
 	'9cbf0586-646c-4554-991b-5745011098b8',
 	'Psihičko nasilje',
 	'52',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
 	'60bb2592-fbad-452b-a0d6-22229287cce8',
 	'Psihičko nasilje',
 	'52',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
 	'ced96890-6de4-4aa1-8b97-2c1b65d51191',
 	'Psihičko nasilje',
 	'52',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
 	'4cda220d-227b-4943-89a7-4d8da239f411',
 	'Psihičko nasilje',
 	'52',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'05d56b7e-6a98-454e-971d-33edf9da6fba',
+	'7e9611eb-515a-47eb-ba39-4f3a1efcbf25',
 	'Fizičko nasilje',
 	'53',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'c1bb417c-9fb9-4748-b34a-438175b940eb',
+	'92e00b55-c364-40c5-bc0b-0ee95fc3061b',
 	'Fizičko nasilje',
 	'53',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'1a08d511-1ad9-49c2-a6ae-f85e62d3802b',
+	'8c4387f2-043f-407a-a411-bcff2258a13a',
 	'Fizičko nasilje',
 	'53',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'f1262eae-e891-4fa9-b38a-d74160aa3158',
+	'cd96b78a-ba13-4672-b08a-81fe7754514a',
 	'Fizičko nasilje',
 	'53',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'7fb2560a-8a41-4c5a-8909-0162564feb50',
+	'0d5900e2-a4f7-4a7a-8a58-05203d616248',
 	'Fizičko nasilje',
 	'53',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'57d68e96-8c0f-450b-838f-e24dee425b1e',
+	'aff4a43e-f554-4a76-84d4-0f7f11ef938e',
 	'Seksualno nasilje',
 	'54',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'f8acff50-5b83-4c8b-8ccb-c50acb9ba741',
+	'd89804cf-2e9a-4e96-8c3b-f05bcdbbd349',
 	'Seksualno nasilje',
 	'54',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'21208820-113b-404e-9c0e-563f6563d919',
+	'a7113b08-21b5-45d8-96a7-49171be9eadd',
 	'Seksualno nasilje',
 	'54',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'07ce4e5d-1012-45b4-833c-7c85b7c0421b',
+	'3b58bd6e-7025-4c8c-a6e2-746247c37598',
 	'Seksualno nasilje',
 	'54',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'c0bea941-0e3a-4560-9ae3-08a016cff2aa',
+	'fbfc6bfd-1a3b-43ff-ac31-167daeb4e1a8',
 	'Seksualno nasilje',
 	'54',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'35d22c5b-8e61-4045-a466-fdc398c92aad',
+	'55216638-66ee-481a-a4fc-80ae2a0beb44',
 	'Ekonomsko nasilje',
 	'55',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'177b28e5-f5d7-4404-bfc3-4a593dee9de6',
+	'ae6aac8b-0868-413f-b850-078ea0e466dc',
 	'Ekonomsko nasilje',
 	'55',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'b26de472-9a47-4588-88d4-b2cff9df512e',
+	'd999aaff-4cb8-4734-b532-34998c8d18fb',
 	'Ekonomsko nasilje',
 	'55',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'f20046d5-b1d3-4045-9055-8f1d64c4c6dd',
+	'f74d6a94-4541-40d5-9b3e-23c9068febdc',
 	'Ekonomsko nasilje',
 	'55',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'40216bee-a26c-4786-bafb-97bc3a610799',
+	'b943d7ad-e585-4035-be9f-c2f14b55a240',
 	'Ekonomsko nasilje',
 	'55',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'f2cfb19a-809a-4afc-8b31-836f4cc14de0',
+	'c4f2a0af-5bcb-47c6-b803-2901e76a0c87',
 	'Digitalno nasilje',
 	'56',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'ad523349-b6f5-4a3a-aff8-98153f4ac1ba',
+	'5fbc8514-10d6-495f-a1e9-0f401ab0de02',
 	'Digitalno nasilje',
 	'56',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'b30167fd-e6d0-40e6-accd-de78d7febac5',
+	'67d34c5c-34d2-41e8-bc32-404ea7c36e51',
 	'Digitalno nasilje',
 	'56',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'c2d641ee-e103-4db9-8511-8a67a11e72be',
+	'05399ce5-88b7-4e98-bc6c-069c4c8d1249',
 	'Digitalno nasilje',
 	'56',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'22579fb0-e4e3-4794-9b60-b2662055008e',
+	'd4c2abfa-93e2-4169-aee3-07cbc441a0a6',
 	'Digitalno nasilje',
 	'56',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'17fcdeb4-f034-4a66-a7bd-22d1f2a4361e',
+	'a6ba0480-18e5-4d13-b6cf-9ec07294ea46',
 	'Proganjanje',
 	'57',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'514c58ed-7947-4a05-bfc1-4facac66857f',
+	'15ed1eed-a6e8-4571-904a-c3421793147c',
 	'Proganjanje',
 	'57',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'0d710eaa-6e37-4bbc-99f7-6ba26a70bbf2',
+	'76329047-485d-42df-972d-3bf2781fb70e',
 	'Proganjanje',
 	'57',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'dff8b75d-04da-4410-918c-254e2986ad24',
+	'3e76ee35-eba4-419d-8e5f-a4e790e9d9eb',
 	'Proganjanje',
 	'57',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'bc441975-7ee9-42bc-8242-d8112be1a7f0',
+	'6adb5b13-d2ef-4cc0-ae9c-d7fc4cba99e7',
 	'Proganjanje',
 	'57',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'd8d3beaa-db11-4372-9a6b-770f10345b7e',
+	'52df7b62-c52f-4741-999b-5f682cfee69e',
 	'Socijalna izolacija',
 	'58',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'0398b3ac-ccc9-447c-8e86-279968cefbb9',
+	'413c1f0a-5ad9-48e7-88eb-5632300c4eaf',
 	'Socijalna izolacija',
 	'58',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'8780f279-792f-4611-9186-217371a22721',
+	'7e2c83e1-baf5-40d3-8f42-ef8620c76395',
 	'Socijalna izolacija',
 	'58',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'7e137672-b90a-4b34-a3e0-8cc96dec461a',
+	'ffa7c534-105a-448e-b692-7ed21cc6ace7',
 	'Socijalna izolacija',
 	'58',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'ce1c9f6f-bd85-4e14-a882-e222f413f5c6',
+	'6a16f701-490a-4be4-b85c-b37882271536',
 	'Socijalna izolacija',
 	'58',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'716b1184-70a1-4a72-b091-f017b60f4f2f',
+	'ffd3d2a7-23a9-4ad6-ad8b-2aa562cc99cc',
 	'Vršnjačko nasilje',
 	'59',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'68e42d29-6fa1-447b-9d2e-0af3fba53993',
+	'776eb5ee-7936-4ae0-b744-26af2e898071',
 	'Vršnjačko nasilje',
 	'59',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'7781a60c-c7e0-4955-b107-79cf125d93b7',
+	'0e3d245e-51c8-4248-96c2-c840ac64cfa1',
 	'Vršnjačko nasilje',
 	'59',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'13a01f70-3b3d-4ff9-89dd-ff2ef9648f88',
+	'604d990e-5360-46f2-9188-a10224dd7701',
 	'Vršnjačko nasilje',
 	'59',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'79efca40-7c9a-4f94-a27e-a2dedbb2f9eb',
+	'be97a321-88df-4c8d-83df-3acf2423ba7e',
 	'Vršnjačko nasilje',
 	'59',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'8d83cbe0-55b7-461d-883b-fe45cd9d4a9d',
+	'ef861e75-c725-45c2-805c-274e49a87cfd',
 	'Institucionalno nasilje',
 	'60',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'70e38b1a-7333-49cc-bcf1-3f4cad79e21e',
+	'6d9dcd98-3cfb-4ce5-96a3-68a7d6a192d2',
 	'Institucionalno nasilje',
 	'60',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'ce6dcdb5-6179-4400-8572-561dd657f12a',
+	'79e9126c-d525-4fc2-a5d2-bef9ba8dc25a',
 	'Institucionalno nasilje',
 	'60',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'145cc9be-40f6-4ed1-b36c-cd3d758d6b02',
+	'04e28f58-6686-4a5a-9c0d-e876e4611c9f',
 	'Institucionalno nasilje',
 	'60',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'7eddc652-11ea-4029-b2e8-9348fa527e14',
+	'4d27cc09-9a46-408d-8ddb-7772b50d3b21',
 	'Institucionalno nasilje',
 	'60',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-----------------------------------------------------------
 (
-	'813cab35-cabc-4fbb-a32f-f5fa98e717a5',
+	'0464a002-a9e2-4f94-b447-1a6be76068e3',
 	'Bivši muž',
 	'61',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'657a5fd3-d9a9-4d61-a392-a54caefd4bb6'
 ),
 (
-	'7190fc9d-1eed-4a2b-ae79-f724599e28b9',
+	'764f1cff-7e30-481f-8cab-243f5302bbe3',
 	'Bivši muž',
 	'61',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'146a9993-70df-4e2c-8cad-4e1c3aee1377'
 ),
 (
-	'c3ccf2d7-2256-4590-8c19-34cbc0d8dd0d',
+	'7c60ba22-9100-404a-8c95-dc937c0517dc',
 	'Bivši muž',
 	'61',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'0c465680-db66-428e-ac6b-6a1754e178ef'
 ),
 (
-	'cd3a5e44-66ed-46f6-b31c-293245c93d88',
+	'79d1ee58-99bb-43f0-b641-c413ae15a240',
 	'Bivši muž',
 	'61',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'cf6c7d5e-0c99-41e0-aa26-113ee257b70f'
 ),
 (
-	'd4085e7a-4406-4a94-ad12-28931a850803',
+	'd66f3f39-8dcd-4ce6-8acd-c4c593cc8d90',
 	'Bivši muž',
 	'61',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'56cb349a-b7bd-4825-9e29-6ffd6c383ab8'
 ),
+-------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------
+
 
 ----------------------------Nasilnik je ranije prijavljen za nasilje-----
 (
 	'0879d9ac-203e-4beb-8dbe-e747a277c48a',
 	'Da',
 	'62',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'70c32d0f-e1ef-46ac-b1c6-78b5d0b10411'
 ),
 (
 	'a5e3ef93-d2a8-4f5a-a1ec-3cbd243ad352',
 	'Da',
 	'62',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'27acb1ae-5ebd-494e-bdcc-f2e5306812c0'
 ),
 (
 	'da5db69d-3bb4-4e34-bb1b-aec8fe62635a',
 	'Da',
 	'62',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'f2379527-e080-41e4-afc5-506c21fa4406'
+),
+(
+	'76e957ab-291e-4788-a160-5d8444821dff',
+	'Da',
+	'62',
+	'ed00c8de-d334-4d00-bc51-180038a955fb'
 ),
 (
 	'c627aabf-7d21-473e-88d1-683cf5e3ec9e',
 	'Da',
 	'62',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'415df83e-685d-4da7-bc56-ab2955fd099b'
 ),
+---------------------------------------------------------
 (
-	'3494ae22-9601-4b00-9d9a-0a5a757fa7ef',
-	'Ne',
-	'62',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
-),
-(
-	'797fc5a5-08e7-4761-a7b1-61f9ea3b2315',
+	'af652d72-3cb6-4d4f-818d-c026f47ece87',
 	'Ne',
 	'63',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'70c32d0f-e1ef-46ac-b1c6-78b5d0b10411'
 ),
 (
-	'16425db0-ff5c-4f2c-a102-3bb1c439fb73',
+	'772a01bc-2e51-4fd5-875e-785096e2099c',
 	'Ne',
 	'63',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'27acb1ae-5ebd-494e-bdcc-f2e5306812c0'
 ),
 (
-	'fe69eafd-e791-4129-b55c-948ec30a292a',
+	'6d80a134-f1d0-4b20-889f-2d1899d58f4d',
 	'Ne',
 	'63',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'f2379527-e080-41e4-afc5-506c21fa4406'
 ),
 (
-	'e3ce742b-60c6-45dd-a360-314d8a03e36e',
+	'bef287dc-894e-4fc7-bb4c-04bf0538cff8',
 	'Ne',
 	'63',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'ed00c8de-d334-4d00-bc51-180038a955fb'
 ),
 (
-	'9cb01786-f171-4ff6-acdc-ac5e176c313a',
+	'9ebe0834-3c54-4830-b9a4-66f20742890f',
 	'Ne',
 	'63',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'415df83e-685d-4da7-bc56-ab2955fd099b'
 ),
+
+--------------------------------------------------------
+
 -------------------Nasilnik ima izrečeno krivično delo nasilja u porodici-----
 (
 	'4e492f4d-fbe2-4528-aa6d-4ed1e3e4728d',
 	'Da',
 	'64',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'69b1fc08-1609-4000-966b-0a750b4bf3d4'
 ),
 (
 	'c62f8f05-7d81-4e4f-a2e1-9f0cf5680081',
 	'Da',
 	'64',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'0cbb6b00-018e-4a1d-8f50-cb54c459207c'
 ),
 (
 	'b1020926-d0d5-447a-8de9-cbf8cfbee433',
 	'Da',
 	'64',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'a62dd43d-0a37-4461-b6b1-aedbb12b41ed'
 ),
 (
 	'8806e0d3-18b1-46ec-b19b-2956bed9943b',
 	'Da',
 	'64',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'e480a779-f47e-4145-8a48-b926d3f6adee'
 ),
 (
 	'9984dede-6031-4543-a996-25a57ba7f7b3',
 	'Da',
 	'64',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'bed1f5ee-51ff-43a6-a310-1a2c44b79e61'
 ),
+-------------------------------------------------------
 (
-	'667bc373-99bd-4ada-be15-92a469fcd535',
+	'abbe323c-1ce8-446f-a7f6-ee232420e50c',
 	'Ne',
 	'65',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'69b1fc08-1609-4000-966b-0a750b4bf3d4'
 ),
 (
-	'b6f55b55-503d-4c58-9077-b8d00385a254',
+	'9a0d71d9-4aec-405b-9a6e-f160f2685c79',
 	'Ne',
 	'65',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'0cbb6b00-018e-4a1d-8f50-cb54c459207c'
 ),
 (
-	'2fae65df-cd7a-4bcd-b50e-c76eae0d5694',
+	'2c853065-4786-47bf-9860-379e149881c1',
 	'Ne',
 	'65',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'a62dd43d-0a37-4461-b6b1-aedbb12b41ed'
 ),
 (
-	'23dcc9a1-abdd-4e01-b773-01e766890f0e',
+	'bae90d6f-adb9-44d2-a65f-c10854d2befe',
 	'Ne',
 	'65',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'e480a779-f47e-4145-8a48-b926d3f6adee'
 ),
 (
-	'dfa38d45-f516-4b2b-a5d0-cc190f87d238',
+	'25471803-1f9e-4e33-ba8e-102e4562e46f',
 	'Ne',
 	'65',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'bed1f5ee-51ff-43a6-a310-1a2c44b79e61'
 ),
+-------------------------------------------------------
 ---------------Nasilnik ima izrečene hitne mere-------------------
 (
 	'e086baa3-5d6b-417f-a0d6-6c97c9b06f53',
 	'Da',
 	'66',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'326cf6c3-cbd1-4fed-bb78-e8a58e963099'
 ),
 (
 	'b5df0738-473d-4ab2-9e8a-1c3145992a86',
 	'Da',
 	'66',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'7ec7607f-9a06-4909-9eef-2be475ab7a9b'
 ),
 (
 	'b9638c4b-5979-451a-b3d2-701ef4c95495',
 	'Da',
 	'66',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'1c0cb9d5-43a9-4318-ab2f-196579398a2c'
 ),
 (
 	'9522de78-55cc-4427-997d-c03a08d5a345',
 	'Da',
 	'66',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'b6e8ca52-433a-49e0-aac2-6e4ddce3e040'
 ),
 (
 	'c449fb82-6d55-48b2-986f-2b2311a16a1e',
 	'Da',
 	'66',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'046a6ede-7845-411b-b444-11e537aa1320'
 ),
+-----------------------------------------------------------
 (
-	'b5971534-7e24-4251-be22-0c8814dd1994',
+	'4ab2a80f-4f54-4bfa-bec6-e5a9e672050a',
 	'Ne',
 	'67',
-	'a2f9cbb5-f93e-47ed-ba06-b4e42f394a0f'
+	'326cf6c3-cbd1-4fed-bb78-e8a58e963099'
 ),
 (
-	'fec1cdd9-3d42-48fc-8b2a-73edae6071f0',
+	'20c30626-99c2-437f-8386-9d6c410813e3',
 	'Ne',
 	'67',
-	'393de121-fcff-4932-bb46-606e4509a302'
+	'7ec7607f-9a06-4909-9eef-2be475ab7a9b'
 ),
 (
-	'872faef8-62b1-4e38-bcb2-596f459c4766',
+	'22a170fd-0325-4745-8de1-948fe02a20fd',
 	'Ne',
 	'67',
-	'b3abe820-f54a-47d3-9882-e6428612e5fc'
+	'1c0cb9d5-43a9-4318-ab2f-196579398a2c'
 ),
 (
-	'13bee2bd-52a4-4fa7-b8e0-52c1de8dcca1',
+	'7316ca04-7983-4470-9a02-471e60d010c9',
 	'Ne',
 	'67',
-	'55d63fe0-90a8-4e60-9592-030731fbf4b4'
+	'b6e8ca52-433a-49e0-aac2-6e4ddce3e040'
 ),
 (
-	'05309975-5340-47bd-a469-afcea7bb1ab3',
+	'42cab9f8-0794-44c7-b469-00902700eb26',
 	'Ne',
 	'67',
-	'a8e718f6-6832-4e57-b387-3349a99cce32'
+	'046a6ede-7845-411b-b444-11e537aa1320'
 );
+-----------------------------------------------------------
