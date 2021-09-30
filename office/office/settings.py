38eccdcdc4e70761a4e73a2ea632ebbe713b892c
Uros Vukovic <u.vukovic@vegait.rs>
2021-09-30 16:33:55 +0200

    secret moved to env

--- office/office/settings.py
+++ office/office/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w-2o20+f!o=3gh)8-6(ah&ps=$t2hs@iy3b(8f1n)avuxqhmoe'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -30,6 +29,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 env = environ.Env()
 environ.Env.read_env(f'{BASE_DIR.parent}/.env')
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env('SECRET_KEY')
 
 # Application definition
 
