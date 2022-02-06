c9b42fe9cc5a77d348c2b4c2aa11eb4463e3402b
Igor Černjak <i.cernjak@vegaitsourcing.rs>
2022-02-06 15:10:06 +0100

    Move Container in its folder

--- sos-web/src/components/pages/Login.styles.js
+++ sos-web/src/components/pages/Login.styles.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Container } from "../layout/Container";
+import { Container } from "../layout/Container/Container";
 
 export const StyledLogin = styled.section`
     & img {
