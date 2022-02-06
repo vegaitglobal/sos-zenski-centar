9324d7793fea26543cd9af923e8266277a887220
Igor Černjak <i.cernjak@vegaitsourcing.rs>
2022-02-06 15:30:49 +0100

    Update the Input component

--- sos-web/src/components/molecules/Input/Input.js
+++ sos-web/src/components/molecules/Input/Input.js
@@ -1,10 +1,18 @@
-import { StyledInput, StyledFormRow, StyledLabel } from './Input.styles';
+import { StyledInput, StyledFormRow, StyledLabel, StyledText } from './Input.styles';
 
 export const Input = ({ label, ...props }) => {
   return (
     <StyledFormRow>
-      <StyledLabel htmlFor={props.id}>{label}</StyledLabel>
+      <StyledLabel htmlFor={props.id} {...props}>
+        {label}
+      </StyledLabel>
       <StyledInput {...props} />
+      {!(props.touched && props.errorMessage && !props.valid)
+        ? null
+        : <StyledText>
+          {props.errorMessage}
+        </StyledText>
+      }
     </StyledFormRow>
   );
 };
--- sos-web/src/components/molecules/Input/Input.styles.js
+++ sos-web/src/components/molecules/Input/Input.styles.js
@@ -6,7 +6,7 @@ export const StyledInput = styled.input`
   width: 100%;
   padding: 11px;
   border-radius: 5px;
-  border: 1px solid ${({ theme }) => theme.color.greyLight};
+  border: 1px solid ${({ theme, valid, touched }) => (!valid && touched) ? theme.color.red : theme.color.greyLight};
   background-color: ${({ theme }) => theme.color.greyLightest};
 
   &:disabled {
@@ -21,11 +21,12 @@ export const StyledFormRow = styled.div`
 
 export const StyledLabel = styled.label`
   display: inline-block;
-  color: ${({ theme }) => theme.color.grey};
+  color: ${({ theme, valid, touched }) => (!valid && touched) ? theme.color.red : theme.color.grey};
   margin-bottom: 6px;
 `;
 
 export const StyledText = styled.span`
   ${typeStyle.medium};
   display: block;
+  margin-top: 10px;
 `;
